68f40784b8665be5f55cae67cbda4821c62c1ee9
Guest User <Guest@epicodus-5.local>
2017-09-26 16:20:31 -0700

    add integration spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,14 @@ require('question')
 
 RSpec.configure do |config|
   config.after(:each) do
+    Survey.all().each() do |survey|
+      survey.destroy()
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.after(:each) do
     Question.all().each() do |question|
       question.destroy()
     end
